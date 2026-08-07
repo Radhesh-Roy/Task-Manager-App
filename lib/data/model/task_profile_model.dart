8ba483b1381b80e95996767b37990d37ea6ddf22
Radhesh-Roy <radheshroy0011@gmail.com>
2026-08-08 00:20:42 +0600

    add

---
+++ lib/data/model/task_profile_model.dart
@@ -0,0 +1,40 @@
+class TaskProfile {
+  String? sId;
+  String? email;
+  String? firstName;
+  String? lastName;
+  String? mobile;
+  String? password;
+  String? createdDate;
+
+  TaskProfile(
+      {this.sId,
+        this.email,
+        this.firstName,
+        this.lastName,
+        this.mobile,
+        this.password,
+        this.createdDate});
+
+  TaskProfile.fromJson(Map<String, dynamic> json) {
+    sId = json['_id'];
+    email = json['email'];
+    firstName = json['firstName'];
+    lastName = json['lastName'];
+    mobile = json['mobile'];
+    password = json['password'];
+    createdDate = json['createdDate'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['_id'] = this.sId;
+    data['email'] = this.email;
+    data['firstName'] = this.firstName;
+    data['lastName'] = this.lastName;
+    data['mobile'] = this.mobile;
+    data['password'] = this.password;
+    data['createdDate'] = this.createdDate;
+    return data;
+  }
+}
