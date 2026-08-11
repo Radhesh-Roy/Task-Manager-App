f36bad58ae03fcfd75648f6fb7b52f4a0c40ec3f
Radhesh-Roy <radheshroy0011@gmail.com>
2026-08-11 22:35:31 +0600

    add

--- lib/data/service/api_caller.dart
+++ lib/data/service/api_caller.dart
@@ -20,7 +20,6 @@ class ApiCaller {
       else{
         return ApiResponse(responseCode: response.statusCode, responseData: jsonDecode(response.body), isSuccess: false, errorMessage: jsonDecode(response.body));
       }
-
     }catch(e){
       return ApiResponse(responseCode: -1, responseData: null, isSuccess: false, errorMessage:e.toString());
 
