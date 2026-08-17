e64ef78068f4fcd0e7318a1841da62c01342754f
Radhesh-Roy <radheshroy0011@gmail.com>
2026-08-17 23:01:57 +0600

    add

--- lib/screen/bottom_bar.dart
+++ lib/screen/bottom_bar.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:task_manager/data/model/task_model.dart';
 import 'package:task_manager/data/model/user_model.dart';
 import 'package:task_manager/data/service/api_caller.dart';
 import 'package:task_manager/screen/cancel_task_view.dart';
@@ -20,51 +16,55 @@ class BottomBarView extends StatefulWidget {
 }
 
 class _BottomBarViewState extends State<BottomBarView> {
-  List<UserModel> profileDetails=[];
+  List<UserModel> profileDetails = [];
+
+  int selectedIndex = 0;
+
+  final List<Widget> screens = [
+    const NewTaskView(),
+    const ProgressTaskView(),
+    const CancelTaskView(),
+    const CompleteTaskView(),
+  ];
+
   @override
   void initState() {
     super.initState();
     getUser();
   }
 
-  Future<void>getUser()async{
-    final response= await ApiCaller().getRequest(URL: TMUrls.profileDetails);
-    List<UserModel> tempList=[];
+  Future<void> getUser() async {
+    final response = await ApiCaller().getRequest(
+      URL: TMUrls.profileDetails,
+    );
 
-    if(response.isSuccess){
-      for(Map<String, dynamic>jsonData in response.responseData['data']){
-        tempList.add(UserModel.fromJson(jsonData));
+    if (response.isSuccess) {
+      List<UserModel> tempList = [];
 
+      for (Map<String, dynamic> jsonData
+      in response.responseData['data']) {
+        tempList.add(
+          UserModel.fromJson(jsonData),
+        );
+      }
+
+      if (!mounted) return;
+
+      setState(() {
+        profileDetails = tempList;
+      });
+    } else {
+      if (!mounted) return;
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            response.responseData["data"].toString(),
+          ),
+        ),
+      );
     }
-    setState(() {
-      profileDetails=tempList;
-    });
-
-    }else{
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(response.responseData["data"])));
-
-    }
-
-    }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
   }
-
-
-  }
-
-
-
-  int selectedIndex=0;
-  List screens=[
-    NewTaskView(),
-    ProgressTaskView(),
-    CancelTaskView(),
-    CompleteTaskView(),
-  ];
 
   @override
   Widget build(BuildContext context) {
@@ -73,36 +73,81 @@ class _BottomBarViewState extends State<BottomBarView> {
         backgroundColor: Colors.green.shade400,
         elevation: 0,
         scrolledUnderElevation: 0,
-        title: Column(
+
+        title: const Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text("Radhesh", style: TextStyle(fontSize: 17, color: Colors.white),),
-            Text("radheshroy0011@gmail.com",style: TextStyle(fontSize: 14, color: Colors.white),),
+            Text(
+              "Radhesh",
+              style: TextStyle(
+                fontSize: 17,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              "radheshroy0011@gmail.com",
+              style: TextStyle(
+                fontSize: 14,
+                color: Colors.white,
+              ),
+            ),
           ],
         ),
+
         leading: Padding(
-          padding: const EdgeInsets.only(left: 20, top: 5, bottom: 5),
+          padding: const EdgeInsets.only(
+            left: 20,
+            top: 5,
+            bottom: 5,
+          ),
           child: InkWell(
-            onTap: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>UpdateProfileView()));
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const UpdateProfileView(),
+                ),
+              );
             },
-            child: CircleAvatar(
-              backgroundImage: AssetImage("assets/splash.png",),
+            child: const CircleAvatar(
+              backgroundImage: AssetImage(
+                "assets/splash.png",
+              ),
             ),
           ),
         ),
       ),
+
       body: screens[selectedIndex],
+
       bottomNavigationBar: NavigationBar(
         selectedIndex: selectedIndex,
-          onDestinationSelected: (int index){
-          selectedIndex=index;
-          },
-          destinations:[
-        NavigationDestination(icon: Icon(Icons.task_outlined), label: "New Task"),
-        NavigationDestination(icon: Icon(Icons.refresh_outlined), label: "Progress"),
-        NavigationDestination(icon: Icon(Icons.cancel_outlined), label: "Cancel"),
-        NavigationDestination(icon: Icon(Icons.task_alt_outlined), label: "Completed"),
-      ])
+
+        onDestinationSelected: (int index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.task_outlined),
+            label: "New Task",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.refresh_outlined),
+            label: "Progress",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.cancel_outlined),
+            label: "Cancel",
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.task_alt_outlined),
+            label: "Completed",
+          ),
+        ],
+      ),
     );
   }
+}
