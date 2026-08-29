1e7de98fd4adda0ba5c9598719605800d99842f4
Radhesh-Roy <radheshroy0011@gmail.com>
2026-08-29 23:27:01 +0600

    Fix All

--- lib/data/model/api_response.dart
+++ lib/data/model/api_response.dart
@@ -4,6 +4,7 @@ class ApiResponse {
   final bool isSuccess;
   final String ? errorMessage;
   ApiResponse({
+
     required this.responseCode,
     required this.responseData,
     required this.isSuccess,
